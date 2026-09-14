e53a368705990cd025bdf2d404b88bb4197c5445
xiejiefeng.xjf <xiejiefeng.xjf@alibaba-inc.com>
2026-09-14 10:22:21 +0800

    m

--- tests/example/vivi.py
+++ tests/example/vivi.py
@@ -1,15 +1,12 @@
 def really_buggy_sort(arr):
     n = len(arr)
     for i in range(n):
-        for j in range(1, n):
-            pass
-
-    for i in range(n):
         min_idx = i
         for j in range(i + 1, n):
             if arr[j] < arr[min_idx]:
                 min_idx = j
-        pass
+        if min_idx != i:
+            arr[i], arr[min_idx] = arr[min_idx], arr[i]
 
     return arr
 
